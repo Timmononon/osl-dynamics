0f96377693a00be42d8a1af64baf43e6e5485831
Timmononon <3057849718@qq.com>
2026-02-02 19:58:46 +0800

    修复变量定义以移除冗余的tf.keras前缀

--- osl_dynamics/inference/layers.py
+++ osl_dynamics/inference/layers.py
@@ -288,11 +288,11 @@ class SampleGammaDistributionLayer(layers.Layer):
         super().__init__(**kwargs)
         self.epsilon = epsilon
         if do_annealing:
-            self.annealing_factor = tf.keras.Variable(
+            self.annealing_factor = tf.Variable(
                 0.0, trainable=False, name="gamma_anneal_factor"
             )
         else:
-            self.annealing_factor = tf.keras.Variable(
+            self.annealing_factor = tf.Variable(
                 1.0, trainable=False, name="gamma_anneal_factor"
             )
 
@@ -366,7 +366,7 @@ class SampleGumbelSoftmaxDistributionLayer(layers.Layer):
 
     def __init__(self, temperature, **kwargs):
         super().__init__(**kwargs)
-        self.temperature = tf.keras.Variable(
+        self.temperature = tf.Variable(
             temperature, trainable=False, dtype=tf.float32, name="gs_temperature"
         )
 
@@ -1266,11 +1266,11 @@ class KLLossLayer(layers.Layer):
     def __init__(self, do_annealing, **kwargs):
         super().__init__(**kwargs)
         if do_annealing:
-            self.annealing_factor = tf.keras.Variable(
+            self.annealing_factor = tf.Variable(
                 0.0, trainable=False, name="kl_factor"
             )
         else:
-            self.annealing_factor = tf.keras.Variable(
+            self.annealing_factor = tf.Variable(
                 1.0, trainable=False, name="kl_factor"
             )
 
